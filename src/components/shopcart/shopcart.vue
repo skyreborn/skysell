7a539a3a7d3f121c853b9ab80bc74f67bf3524e3
skyreborn <790674452@qq.com>
2019-08-12 15:25:37 +0800

    完成购物车详情页展示，重点动画过渡

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,34 +1,60 @@
 <template>
-	<div class="shopcart">
-		<div class="content">
-			<div class="content-left">
-				<div class="logo-wrapper">
-					<div class="logo" :class="{'highlight':totalCount>0}">
-						<i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
+	<div>
+		<div class="shopcart">
+			<div class="content">
+				<div class="content-left">
+					<div class="logo-wrapper">
+						<div class="logo" :class="{'highlight':totalCount>0}" @click="toggleList">
+							<i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
+						</div>
+						<div class="num" v-show="totalCount>0">{{ totalCount }}</div>
 					</div>
-					<div class="num" v-show="totalCount>0">{{ totalCount }}</div>
+					<div class="price" :class="{'highlight':totalCount>0}">¥{{ totalPrice }}</div>
+					<div class="desc">另需配送费¥{{ deliveryPrice }}元</div>
 				</div>
-				<div class="price" :class="{'highlight':totalCount>0}">¥{{ totalPrice }}</div>
-				<div class="desc">另需配送费¥{{ deliveryPrice }}元</div>
-			</div>
-			<div class="content-right">
-				<div class="pay" :class="payClass">
-					{{ payDesc }}
-				</div>
-			</div>
-		</div>
-		<div class="ball-container">
-			<div v-for="(ball,index) in balls" :key="index">
-				<transition
-					@before-enter="beforeDrop"
-					@enter="dropping"
-					@after-enter="afterDrop">
-					<div class="ball" v-show="ball.show">
-					<div class="inner inner-hook"></div>
+				<div class="content-right" @click="pay">
+					<div class="pay" :class="payClass">
+						{{ payDesc }}
 					</div>
-				</transition>
+				</div>
 			</div>
+			<div class="ball-container">
+				<div v-for="(ball,index) in balls" :key="index">
+					<transition
+						@before-enter="beforeDrop"
+						@enter="dropping"
+						@after-enter="afterDrop">
+						<div class="ball" v-show="ball.show">
+						<div class="inner inner-hook"></div>
+						</div>
+					</transition>
+				</div>
+			</div>
+			<transition name="move">
+				<div class="shopcart-list" v-show="listShow">
+					<div class="list-header">
+						<h1 class="title">购物车</h1>
+						<span class="empty" @click="empty">清空</span>
+					</div>
+					<div class="list-content" ref="listContent">
+						<ul>
+							<li class="food" v-for="food in selectFoods">
+								<span class="name">{{ food.name }}</span>
+								<div class="price">
+									<span>¥{{ food.price*food.count }}</span>
+								</div>
+								<div class="cartcontrol-wrapper">
+									<carcontrol :food="food"></carcontrol>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</transition>
 		</div>
+		<transition name="fade">
+			<div class="list-mask" @click="hideList" v-show="listShow"></div>
+		</transition>
 	</div>
 </template>
 
@@ -43,6 +69,8 @@
 		}
 		return balls
 	}
+	import carcontrol from '@/components/cartcontrol/cartcontrol'
+	import BScroll from 'better-scroll'
 	export default {
 		props: {
 			selectFoods: {
@@ -62,7 +90,8 @@
 		},
 		data() {
 			return {
-				balls: createBalls()
+				balls: createBalls(),
+				fold: true
 			}
 		},
 		created() {
@@ -99,6 +128,25 @@
 				}else {
 					return `enough`
 				}
+			},
+			listShow() {
+				if( !this.totalCount) {
+					this.fold = true
+					return false
+				}
+				let show = !this.fold
+				if(show) {
+					this.$nextTick(() => {
+						if(!this.scroll) {
+							this.scroll = new BScroll(this.$refs.listContent,{
+								click: true
+							})
+						}else {
+							this.scroll.refresh()
+						}
+					})
+				}
+				return show
 			}
 		},
 		methods: {
@@ -136,12 +184,36 @@
 					ball.show = false
 					el.style.display = 'none'
 				}
+			},
+			toggleList() {
+				if(!this.totalCount) {
+					return
+				}
+				this.fold = !this.fold
+			},
+			hideList() {
+				this.fold = true
+			},
+			empty() {
+				this.selectFoods.forEach((food) => {
+					food.count = 0
+				})
+			},
+			pay() {
+				if(this.totalPrice < this.minPrice) {
+					return
+				}
+				window.alert(`支付${this.totalPrice}元`)
 			}
+		},
+		components: {
+			carcontrol
 		}
 	}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixin.styl"
 .shopcart
 	position: fixed
 	left: 0
@@ -242,4 +314,73 @@
 			border-radius: 50%
 			background: rgb(0,160,220)
 			transition: all 0.4s linear
+	.shopcart-list
+		position: absolute
+		left: 0
+		top: 0
+		z-index: -1
+		width: 100%
+		transition: all 0.8s
+		transform: translate3d(0, -100%, 0)
+		&.move-enter, &.move-leave-active
+			transform: translate3d(0, -100%, 0)
+		&.move-enter-active, &.move-leave-active
+			transition: all 0.8s
+			transform: translate3d(0, 0, 0)
+		.list-header
+			height: 40px
+			line-height:40px
+			padding: 0 18px
+			background:#f3f5f7
+			border-bottom: 1px solid rgba(7,17,27,0.1)
+			.title
+				float: left
+				font-size: 14px
+				color: rgb(7,17,27)
+			.empty
+				float: right
+				font-size: 12px
+				color: rgb(0,160,220)
+		.list-content
+			padding: 0 18px
+			max-height: 217px
+			overflow: hidden
+			background: #fff
+			.food
+				position: relative
+				padding: 12px 0
+				box-sizing: border-box
+				border-1px(rgba(7,17,27,0.1))
+				.name
+					line-height: 24px
+					font-size: 14px
+					color: rgb(7,17,27)
+				.price
+					position: absolute
+					right: 90px
+					bottom: 12px
+					line-height: 24px
+					font-size: 14px
+					font-weight: 700
+					color: rgb(240,20,20)
+				.cartcontrol-wrapper
+					position:absolute
+					right:0
+					bottom: 6px
+.list-mask
+	position: fixed
+	top: 0
+	left: 0
+	width: 100%
+	height: 100%
+	z-index: 40
+	backdrop-filter: blur(10px)
+	opacity: 1
+	background: rgba(7,17,27,0.6)//渐变结束后的最终效果	
+	&.fade-enter-active, &.fade-leave-active//渐变的进入和退出都历时0.5s
+		transition: all 1s
+		background: rgba(7,17,27,0.6)
+	&.fade-enter, &.fade-leave-to//定义进入前和退出后样式，即透明无色背景。
+		opacity: 0
+		background: rgba(7,17,27,0)
 </style>
